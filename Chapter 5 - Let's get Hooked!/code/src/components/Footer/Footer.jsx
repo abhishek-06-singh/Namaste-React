998c7e1096b1d9bd2d02ae73e06311df377d5036
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-05-08 12:10:32 +0530

    Update Footer.jsx

---
+++ Chapter 5 - Let's get Hooked!/code/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css"
+const Firstfooter = () => {
+  return (
+    <div className='firfoot'>
+        <h3>Made as an Assignment in 😃 Nameste react 💖 </h3>
+    </div>
+  )
+}
+
+export default Firstfooter

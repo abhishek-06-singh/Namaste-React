4b28fbad8bc145c394edf46b336c8c20e2b90a1c
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-05-08 13:34:56 +0530

    commit

--- Chapter 5 - Let's get Hooked!/code/src/components/Body/Body.jsx
+++ Chapter 5 - Let's get Hooked!/code/src/components/Body/Body.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react"
 import "./body.css"
 import Res from "../../assets/data"
@@ -6,20 +5,40 @@ import RestaurantCard from './RestaurantCard'
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(Res)
-    return(
-        <div className="body-">
-          <button className="button-89" onClick={()=>{
-      console.log('clicked')
-    }}>
-        Click to Sort 
-    </button>
-          <div className="restaurant-list">
-         {listOfRestaurant.map((restaurant) => {
-           return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-      })}
-          </div>
-        </div>
-    
-    )
+  return (
+    <div className="body-">
+      <button className="button-57"
+        onClick={() => {
+          const filterlist = listOfRestaurant.filter(
+            (Res) => Res.data.avgRating > 4
+          )
+          setListOfRestaurant(filterlist);
+        }}>
+        <span className="text">
+        </span>
+        <span>
+          Restaurants with 4+ Ratings
+        </span>
+        Click
+      </button>
+      <button className="button-57"
+        onClick={() => {
+          setListOfRestaurant(Res);
+        }}>
+        <span className="text">
+        </span>
+        <span>
+          All Restaurants
+        </span>
+        Click
+      </button>
+      <div className="restaurant-list">
+        {listOfRestaurant.map((restaurant) => {
+          return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
+        })}
+      </div>
+    </div>
+  )
 }
+
 export default Body

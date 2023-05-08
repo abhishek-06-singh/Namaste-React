e718c74d04f2914c3b2f9caeedea29d6d37952b4
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-05-08 12:31:19 +0530

    Update Body.jsx

--- Chapter 5 - Let's get Hooked!/code/src/components/Body/Body.jsx
+++ Chapter 5 - Let's get Hooked!/code/src/components/Body/Body.jsx
@@ -2,9 +2,11 @@
 import "./body.css"
 import Res from "../../assets/data"
 import RestaurantCard from './RestaurantCard'
+import Button from "../Button/Button"
 const Body = () => {
     return(
         <div className="restaurant-list">
+            <Button />
       {Res.map((restaurant) => {
         return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
        

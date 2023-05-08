608fa9b4b9cc75ac34b67fcc29a82a673831ffa8
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-05-08 11:56:13 +0530

    commit

---
+++ Chapter 5 - Let's get Hooked!/code/src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+
+import './header.css'
+import Logo from './logo.png'
+
+const Header = () => {
+  return (
+    <div className='nav'>
+            <img src={Logo} className='logo' alt="" />
+            <input 
+            className="searchbar"
+            type="text" 
+            placeholder='Search Restaurants'/>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Services
+                </li>
+            </ul>
+    </div>
+  )
+}
+
+export default Header
